39898cfbdf4c626dde72c65013944bf9e13425d9
Filho, Adilson A <adilson.a.filho@accenture.com>
2019-05-01 17:46:24 -0300

    ajustes finais

--- main.py
+++ main.py
@@ -12,13 +12,13 @@ def vertical(img):
     vertical = np.copy(binarized)
     # Pegamos a dimensão do array
     rows = vertical.shape[0]
-    # O valor 20 foi através de experimentação e nos deu um melhor resultado final, quanto menor o valor maior foi a abstração do resultado final.
-    # Para a imagem 4 o melhor valor ficou entre 90~100
-    # um bom trabalho futuro seria a predição do melhor valor.
+    # O valor 20 foi através de experimentação e nos deu um melhor resultado final,
+    # quanto menor o valor maior foi a abstração do resultado final.
+    # Para a imagem 4 o melhor valor ficou entre 90~100.
     verticalsize = rows / 20
     # Pegamos a estrutura do elemento/imagem
     verticalStructure = cv.getStructuringElement(cv.MORPH_CROSS, (1, int(verticalsize)))
-    # Aplicamos a erosão na imagem (valorizamos os pixels com os menores valores, no caso, entre 0 e 1 sempre será escolhido o 0)
+    # Aplicamos a erosão na imagem (valorizamos os pixels com os menores valores, entre 0 e 1 sempre será escolhido o menor)
     vertical = cv.erode(vertical, verticalStructure)
     # Em seguida aplicamos a dilatação que é o inverso da Erosão, justamente para realçarmos os pontos das notas.
     vertical = cv.dilate(vertical, verticalStructure)
@@ -31,17 +31,17 @@ def horizontal(img):
     bw = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 15, -2)
     horizontal = np.copy(bw)
     cols = horizontal.shape[1]
-    horizontal_size = cols / 10
+    horizontal_size = cols / 15
     horizontalStructure = cv.getStructuringElement(cv.MORPH_RECT, (int(horizontal_size), 1))
     horizontal = cv.erode(horizontal, horizontalStructure)
     horizontal = cv.dilate(horizontal, horizontalStructure)
     cv.imshow("FINAL", horizontal)
     cv.waitKey(0)
 
-colored = cv.imread('dataset/1.png', cv.IMREAD_COLOR)
+colored = cv.imread('dataset/3.png', cv.IMREAD_COLOR)
 img = cv.cvtColor(colored, cv.COLOR_BGR2GRAY)
 gray = cv.bitwise_not(img)
 
 vertical(gray)
 # Extra Horizontal
-# horizontal(gray)
+horizontal(gray)
