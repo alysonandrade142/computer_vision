bc626c21f6ca06ff6fea84ac475f8c7cb5e738b2
Filho, Adilson A <adilson.a.filho@accenture.com>
2019-05-01 17:24:09 -0300

    projeto

--- main.py
+++ main.py
@@ -2,57 +2,44 @@ from matplotlib import pyplot as plt
 import cv2 as cv
 import numpy as np
 
-colored = cv.imread('sheets/TP5_AltoTest_600.png', cv.IMREAD_COLOR)
+def vertical(img):
+
+    # Binarizamos a imagem
+    binarized = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 15, -2)
+    # Criamos uma cópia
+    vertical = np.copy(binarized)
+    # Pegamos a dimensão do array
+    rows = vertical.shape[0]
+    # O valor 20 foi através de experimentação e nos deu um melhor resultado final, quanto menor o valor maior foi a abstração do resultado final.
+    # Para a imagem 4 o melhor valor ficou entre 90~100
+    # um bom trabalho futuro seria a predição do melhor valor.
+    verticalsize = rows / 20
+    # Pegamos a estrutura do elemento/imagem
+    verticalStructure = cv.getStructuringElement(cv.MORPH_CROSS, (1, int(verticalsize)))
+    # Aplicamos a erosão na imagem (valorizamos os pixels com os menores valores, no caso, entre 0 e 1 sempre será escolhido o 0)
+    vertical = cv.erode(vertical, verticalStructure)
+    # Em seguida aplicamos a dilatação que é o inverso da Erosão, justamente para realçarmos os pontos das notas.
+    vertical = cv.dilate(vertical, verticalStructure)
+    # RESULTADO
+    cv.imshow("FINAL", vertical)
+    
+    cv.waitKey(0)
+
+def horizontal(img):
+    bw = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 15, -2)
+    horizontal = np.copy(bw)
+    cols = horizontal.shape[1]
+    horizontal_size = cols / 10
+    horizontalStructure = cv.getStructuringElement(cv.MORPH_RECT, (int(horizontal_size), 1))
+    horizontal = cv.erode(horizontal, horizontalStructure)
+    horizontal = cv.dilate(horizontal, horizontalStructure)
+    cv.imshow("FINAL", horizontal)
+    cv.waitKey(0)
+
+colored = cv.imread('dataset/1.png', cv.IMREAD_COLOR)
 img = cv.cvtColor(colored, cv.COLOR_BGR2GRAY)
 gray = cv.bitwise_not(img)
-bw = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 15, -2)
-horizontal = np.copy(bw)
-vertical = np.copy(bw)
 
-def show_wait_destroy(winname, img):
-    cv.imshow(winname, img)
-    cv.moveWindow(winname, 500, 0)
-    cv.waitKey(0)
-    cv.destroyWindow(winname)
-
-# Specify size on vertical axis
-rows = vertical.shape[0]
-verticalsize = rows/30
-# Create structure element for extracting vertical lines through morphology operations
-verticalStructure = cv.getStructuringElement(cv.MORPH_RECT, (1, int(verticalsize)))
-# Apply morphology operations
-vertical = cv.erode(vertical, verticalStructure)
-vertical = cv.dilate(vertical, verticalStructure)
-# Show extracted vertical lines
-show_wait_destroy("vertical", vertical)
-
-# Inverse vertical image
-vertical = cv.bitwise_not(vertical)
-show_wait_destroy("vertical_bit", vertical)
-'''
-Extract edges and smooth image according to the logic
-1. extract edges
-2. dilate(edges)
-3. src.copyTo(smooth)
-4. blur smooth img
-5. smooth.copyTo(src, edges)
-'''
-# Step 1
-edges = cv.adaptiveThreshold(vertical, 255, cv.ADAPTIVE_THRESH_MEAN_C, \
-                            cv.THRESH_BINARY, 3, -2)
-show_wait_destroy("edges", edges)
-# Step 2
-kernel = np.ones((2, 2), np.uint8)
-edges = cv.dilate(edges, kernel)
-show_wait_destroy("dilate", edges)
-# Step 3
-smooth = np.copy(vertical)
-# Step 4
-smooth = cv.blur(smooth, (2, 2))
-# Step 5
-(rows, cols) = np.where(edges != 0)
-vertical[rows, cols] = smooth[rows, cols]
-# Show final result
-show_wait_destroy("smooth - final", vertical)
-
-
+vertical(gray)
+# Extra Horizontal
+# horizontal(gray)
